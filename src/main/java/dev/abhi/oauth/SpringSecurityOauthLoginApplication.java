af0e77a7da688525c896d96b6c73ff340424b571
abhimanyu <abhimanyu.kumar@glaucuslogistics.com>
2020-04-11 16:23:35 +0530

    Inital Commit

---
+++ src/main/java/dev/abhi/oauth/SpringSecurityOauthLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.abhi.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/abhi/oauth/SpringSecurityOauthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abhi.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
